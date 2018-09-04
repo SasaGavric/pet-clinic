726bea5d6e1832771cf53794a0495999c5c7bace
Sasa Gavric <sasa.gavric@asioso.com>
2018-09-04 10:39:22 +0200

    add parent mapping for vet controller

--- src/main/java/com/sasagavric/petclinic/controller/VetController.java
+++ src/main/java/com/sasagavric/petclinic/controller/VetController.java
@@ -2,11 +2,13 @@ package com.sasagavric.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vet")
 public class VetController {
 
-    @GetMapping({"/vets", "/vet/vets", "/vet/vets.html"})
+    @GetMapping({"", "/", "/vets", "/vets.html"})
     public String listVets(){
         return "vet/vets";
     }
